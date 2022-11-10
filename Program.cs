7d7074cd5f15fc3891299b6d87413c03dcff976e
unwar193 <varan193@mail.ru>
2022-11-10 05:10:21 +0300

    added filling of init array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,36 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
-//либо задать на старте выполнения алгоритма. При решении не рекоменндуется пользоваться коллекциями, 
-//лучше обойтись исключительно массивами.
+// либо задать на старте выполнения алгоритма. При решении не рекоменндуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+Console.Clear();
+
+Console.Write("введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+string[] array = new string[num];
+FillArray(array);
+PrintArray(array);
+
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {   
+        Console.WriteLine("Введите данные:");
+        array[i] = Console.ReadLine()!;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"\"" + array[i] + "\"");
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+}
